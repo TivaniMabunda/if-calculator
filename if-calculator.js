16153963382976cdb3770c3c6d9f332fea282841
TivaniMabunda <95340204+TivaniMabunda@users.noreply.github.com>
2022-04-18 16:06:32 +0200

    if calcu

---
+++ if-calculator.js
@@ -0,0 +1,45 @@
+function submitFunction() {
+    let inputBox1 = document.getElementById('input1');
+    let inputBox1value = inputBox1.value; // The value
+    let solution;
+    console.log(inputBox1);
+    console.log(inputBox1value);
+
+    let inputBox2 = document.getElementById('input2');
+    let inputBox2value = inputBox2.value;
+    console.log(inputBox2);
+    console.log(inputBox2value);
+
+    let inputBox3 = document.getElementById('input3');
+    let inputBox3value = inputBox3.value;
+    console.log(inputBox3);
+    console.log(inputBox3value);
+
+    if (inputBox3value === '+') {
+        
+    }
+
+    if (inputBox3value === '-') {
+        
+    }
+
+    if (inputBox3value === '*') {
+        
+    }
+
+    if (inputBox3value === '**') {
+        
+    }
+
+    if (inputBox3value === '/') {
+        
+    }
+
+    if (inputBox3value === '%') {
+        
+    }
+
+    else{
+        alert("Entered Value invalid")
+    }
+}
